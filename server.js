1e7e38abdfefc8b65447825dde51f5dd4435a575
Ulugbek Nazarov <ulugbeknazarov15@gmail.com>
2022-08-05 09:57:38 +0500

    Created project layout

--- server.js
+++ server.js
@@ -2,102 +2,23 @@ const express = require('express')
 const app = express()
 const port = 3000
 const bodyParser = require('body-parser')
-const fs = require('fs')
+const studentControllers = require('./controllers/students')
 
 app.use(bodyParser.json())
 
-const studentsFileName = "students.json"
+app.get("/students", studentControllers.getAllStudents)
 
-var students = []
-students = readFromJSONFile(studentsFileName)
+app.get("/students/:index", studentControllers.getOneStudentByIndex)
 
-app.get("/students", (req, res) => {
-    students = readFromJSONFile(studentsFileName)
-    res.json(students)
-})
+app.get("/students/username/:username", studentControllers.getOneStudentByUsername)
 
-app.get("/students/:index", (req, res) => {
-    students = readFromJSONFile(studentsFileName)
-    var index = req.params.index
-    if (students[index] !== undefined) {
-        res.json(students[index])
-    } else {
-        res.status(404)
-        res.json({ "message": "Not found" })
-    }
-})
+app.post("/students", studentControllers.createStudent)
 
-app.get("/students/username/:username", (req, res) => {
-    var student;
-    username = req.params.username
-    student = students.find(function (element) {
-        return element["username"] === username
-    })
+app.put("/students/:index", studentControllers.updateStudent)
 
-    if (student !== undefined) {
-        res.json(student)
-    } else {
-        res.status(404)
-        res.json({ "message": "Not found" })
-    }
-})
+app.patch("/students/:index/address", studentControllers.updateStudentAddress)
 
-app.post("/students", (req, res) => {
-    students.push(req.body)
-    saveInJSONFile(students, studentsFileName)
-    res.sendStatus(200)
-})
-
-app.put("/students/:index", (req, res) => {
-    var index = req.params.index
-    if (students[index] !== undefined) {
-        students[index] = req.body
-        saveInJSONFile(students, studentsFileName)
-        res.sendStatus(200)
-    } else {
-        res.status(404)
-        res.json({ "message": "Not found" })
-    }
-})
-
-app.patch("/students/:index/address", (req, res) => {
-    var index = req.params.index
-    if (students[index] !== undefined && students[index].hasOwnProperty("address")) {
-        students[index]["address"] = req.body
-        saveInJSONFile(students, studentsFileName)
-        res.sendStatus(200)
-    } else {
-        res.status(404)
-        res.json({ "message": "Not found" })
-    }
-})
-
-app.delete("/students/:index", (req, res) => {
-    var index = req.params.index
-    if (students[index] !== undefined) {
-        students.splice(index, 1)
-        saveInJSONFile(students, studentsFileName)
-        res.sendStatus(200)
-    } else {
-        res.status(404)
-        res.json({ "message": "Not found" })
-    }
-})
-
-function saveInJSONFile(data, fileName) {
-    var jsonData = JSON.stringify(data, null, "\t")
-    fs.writeFile(fileName, jsonData, function (err) {
-        if (err) {
-            console.log(err)
-        }
-    })
-}
-
-function readFromJSONFile(fileName) {
-    var jsonData = fs.readFileSync(fileName, 'utf8');
-    var data = JSON.parse(jsonData)
-    return data
-}
+app.delete("/students/:index", studentControllers.deleteStudent) 
 
 app.listen(port, () => {
     console.log("Listening on port", port)
